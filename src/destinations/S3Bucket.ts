89a85d4f2e74bd10dd90956ccd48c4490bcf9732
dianadevargas <dianadevargas@yahoo.com.mx>
2017-08-01 10:50:23 +1000

    use aws root credencials

--- src/destinations/S3Bucket.ts
+++ src/destinations/S3Bucket.ts
@@ -40,12 +40,13 @@ export class S3Bucket extends EtlDestination {
         s3RetryDelay: 1000, // this is the default
         multipartUploadThreshold: 20971520, // this is the default (20 MB)
         multipartUploadSize: 15728640, // this is the default (15 MB)
-        s3Options: {
-            accessKeyId: 'AKIAIP74ASJ56ISBYJWQ',
-            secretAccessKey: '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',
-            // any other options are passed to new AWS.S3()
-            // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
-        },
+        // If not using the credential stored in ./aws/credencials file use the follow:
+        // s3Options: {
+        //     accessKeyId: 'ACCESS_KEY',
+        //     secretAccessKey: 'ACCESS_SECRET',
+        //     // any other options are passed to new AWS.S3()
+        //     // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+        // },
     };
     this.s3Client = S3.createClient(options);
   }
